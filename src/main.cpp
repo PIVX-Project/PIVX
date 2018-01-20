088f19c4c0b39f1b43e791eebe262e24187a343e
The Cryptocurrency Project <35343785+cryptocurrency-project@users.noreply.github.com>
2018-01-20 04:28:12 -0800

    Update codebase
    More prep for fork - TODO: DECIDE REWARD STRUCTURE

--- src/main.cpp
+++ src/main.cpp
@@ -2182,7 +2182,7 @@ int64_t GetBlockValue(int nHeight)
     }
 
     if (nHeight == 0) {
-        nSubsidy = 60001 * COIN;
+        nSubsidy = 1 * COIN;
     } else if (nHeight < 86400 && nHeight > 0) {
         nSubsidy = 250 * COIN;
     } else if (nHeight < (Params().NetworkID() == CBaseChainParams::TESTNET ? 145000 : 151200) && nHeight >= 86400) {
