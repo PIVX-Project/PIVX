391fd4588616508229fc74456ee572eeaebbec71
cevap <dev@i2pmail.org>
2019-02-09 09:37:40 +0100

    Rename and optimize pivx artworks (bitcoin to pivx)
    - exclude ui.rc from lint-whitespace check

--- doc/assets-attribution.md
+++ doc/assets-attribution.md
@@ -97,7 +97,9 @@ Jonas Schnelli
 * License: MIT
 
 ### Assets Used
-	src/qt/res/icons/bitcoin.icns, src/qt/res/src/bitcoin.svg,
-	src/qt/res/src/bitcoin.ico, src/qt/res/src/bitcoin.png,
-	src/qt/res/src/bitcoin_testnet.png, docs/bitcoin_logo_doxygen.png,
+	src/qt/res/icons/pivx.icns, src/qt/res/src/pivx.svg,
+	src/qt/res/src/pivx.ico, src/qt/res/src/pivx.png,
+	src/qt/res/src/pivx_testnet.png, docs/pivx_logo_doxygen.png,
 	src/qt/res/images/splash.png, src/qt/res/images/splash_testnet.png
+	src/qt/res/images/splash_regtest.png src/qt/res/src/pivx_regtest.png
+	src/qt/res/src/pivx_regtest.ico
--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # create multiresolution windows icon
 #mainnet
-ICON_SRC=../../src/qt/res/icons/bitcoin.png
-ICON_DST=../../src/qt/res/icons/bitcoin.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/pivx.png
+ICON_DST=../../src/qt/res/icons/pivx.ico
+convert ${ICON_SRC} -resize 16x16 pivx-16.png
+convert ${ICON_SRC} -resize 32x32 pivx-32.png
+convert ${ICON_SRC} -resize 48x48 pivx-48.png
+convert pivx-16.png pivx-32.png pivx-48.png ${ICON_DST}
 #testnet
-ICON_SRC=../../src/qt/res/icons/bitcoin_testnet.png
-ICON_DST=../../src/qt/res/icons/bitcoin_testnet.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
-rm bitcoin-16.png bitcoin-32.png bitcoin-48.png
+ICON_SRC=../../src/qt/res/icons/pivx_testnet.png
+ICON_DST=../../src/qt/res/icons/pivx_testnet.ico
+convert ${ICON_SRC} -resize 16x16 pivx-16.png
+convert ${ICON_SRC} -resize 32x32 pivx-32.png
+convert ${ICON_SRC} -resize 48x48 pivx-48.png
+convert pivx-16.png pivx-32.png pivx-48.png ${ICON_DST}
+rm pivx-16.png pivx-32.png pivx-48.png
 #regtest
-ICON_SRC=../../src/qt/res/icons/bitcoin_regtest.png
-ICON_DST=../../src/qt/res/icons/bitcoin_regtest.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
-rm bitcoin-16.png bitcoin-32.png bitcoin-48.png
+ICON_SRC=../../src/qt/res/icons/pivx_regtest.png
+ICON_DST=../../src/qt/res/icons/pivx_regtest.ico
+convert ${ICON_SRC} -resize 16x16 pivx-16.png
+convert ${ICON_SRC} -resize 32x32 pivx-32.png
+convert ${ICON_SRC} -resize 48x48 pivx-48.png
+convert bitcoin-16.png bitcoin-32.png pivx-48.png ${ICON_DST}
+rm pivx-16.png pivx-32.png pivx-48.png
