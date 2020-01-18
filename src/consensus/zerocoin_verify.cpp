5a44e28af111f23926f261d301d8b793a7e3b71d
barrystyle <barrystyle@westnet.com.au>
2020-01-19 02:39:10 +0800

    Further rework/includes required after 4.0.2 rebase

--- src/consensus/zerocoin_verify.cpp
+++ src/consensus/zerocoin_verify.cpp
@@ -14,6 +14,8 @@
 #include "spork.h"               // for sporkManager
 #include "txdb.h"                // for class CZerocoinDB
 #include "utilmoneystr.h"        // for FormatMoney
+#include "zpiv/accumulators.h"   // for CalculateAccumulatorCheckpoint
+#include "zpiv/accumulatormap.h" // for AccumulatorMap
 
 bool CheckZerocoinMint(const uint256& txHash, const CTxOut& txout, CValidationState& state, bool fCheckOnly)
 {
