f7cc86fae00dd5dd1b12342a56e02cff1c9b62fc
Alastair Clark <admin@alastair.guru>
2023-08-21 03:20:29 -0400

    Change logos and splash

--- src/qt/hemis/hemisgui.h
+++ src/qt/hemis/hemisgui.h
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-// Copyright (c) 2019-2022 The PIVX Core developers
+// Copyright (c) 2019-2022 The HEMIS Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef PIVX_CORE_NEW_GUI_PIVXGUI_H
-#define PIVX_CORE_NEW_GUI_PIVXGUI_H
-
-#if defined(HAVE_CONFIG_H)
-#include "config/pivx-config.h"
-=======
-// Copyright (c) 2019-2022 The hemis Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-#ifndef hemis_CORE_NEW_GUI_hemisGUI_H
-#define hemis_CORE_NEW_GUI_hemisGUI_H
+#ifndef HEMIS_CORE_NEW_GUI_hemisGUI_H
+#define HEMIS_CORE_NEW_GUI_hemisGUI_H
 
 #if defined(HAVE_CONFIG_H)
 #include "config/hemis-config.h"
->>>>>>> e92f29f73 (init)
 #endif
 
 #include <QMainWindow>
@@ -26,20 +14,6 @@
 #include <QSystemTrayIcon>
 #include <QLabel>
 
-<<<<<<< HEAD
-#include "qt/pivx/navmenuwidget.h"
-#include "qt/pivx/topbar.h"
-#include "qt/pivx/dashboardwidget.h"
-#include "qt/pivx/send.h"
-#include "qt/pivx/receivewidget.h"
-#include "qt/pivx/addresseswidget.h"
-#include "qt/pivx/coldstakingwidget.h"
-#include "qt/pivx/governancewidget.h"
-#include "qt/pivx/masternodeswidget.h"
-#include "qt/pivx/snackbar.h"
-#include "qt/pivx/settings/settingswidget.h"
-#include "qt/pivx/settings/settingsfaqwidget.h"
-=======
 #include "qt/hemis/navmenuwidget.h"
 #include "qt/hemis/topbar.h"
 #include "qt/hemis/dashboardwidget.h"
@@ -52,7 +26,6 @@
 #include "qt/hemis/snackbar.h"
 #include "qt/hemis/settings/settingswidget.h"
 #include "qt/hemis/settings/settingsfaqwidget.h"
->>>>>>> e92f29f73 (init)
 #include "qt/rpcconsole.h"
 
 namespace interfaces {
@@ -66,30 +39,18 @@ class WalletModel;
 
 
 /**
-<<<<<<< HEAD
-  PIVX GUI main class. This class represents the main window of the PIVX UI. It communicates with both the client and
-  wallet models to give the user an up-to-date view of the current core state.
-*/
-class PIVXGUI : public QMainWindow
-=======
-  hemis GUI main class. This class represents the main window of the hemis UI. It communicates with both the client and
+  HEMIS GUI main class. This class represents the main window of the HEMIS UI. It communicates with both the client and
   wallet models to give the user an up-to-date view of the current core state.
 */
 class hemisGUI : public QMainWindow
->>>>>>> e92f29f73 (init)
 {
     Q_OBJECT
 
 public:
     static const QString DEFAULT_WALLET;
 
-<<<<<<< HEAD
-    explicit PIVXGUI(const NetworkStyle* networkStyle, QWidget* parent = 0);
-    ~PIVXGUI();
-=======
     explicit hemisGUI(const NetworkStyle* networkStyle, QWidget* parent = 0);
     ~hemisGUI();
->>>>>>> e92f29f73 (init)
 
     /** Set the client model.
         The client model represents the part of the core that communicates with the P2P network, and is wallet-agnostic.
@@ -240,8 +201,4 @@ Q_SIGNALS:
 };
 
 
-<<<<<<< HEAD
-#endif //PIVX_CORE_NEW_GUI_PIVXGUI_H
-=======
-#endif //hemis_CORE_NEW_GUI_hemisGUI_H
->>>>>>> e92f29f73 (init)
+#endif //HEMIS_CORE_NEW_GUI_hemisGUI_H
