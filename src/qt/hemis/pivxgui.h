abd11d67378ff2e7f0cc163c23a626261cb4621d
Alastair Clark <admin@alastair.guru>
2023-08-22 04:27:20 -0400

    Revert "Typo"
    This reverts commit 8dec64fede15042530ec0813157284b50c026c28.

--- src/qt/hemis/pivxgui.h
+++ src/qt/hemis/pivxgui.h
@@ -109,7 +109,7 @@ public Q_SLOTS:
     void goToSend();
     void goToReceive();
     void goToAddresses();
-    void goToGamemasters();
+    void goToGamemaSters();
     void goToGovernance();
     void goToColdStaking();
     void goToSettings();
@@ -180,7 +180,7 @@ private:
     SendWidget *sendWidget = nullptr;
     ReceiveWidget *receiveWidget = nullptr;
     AddressesWidget *addressesWidget = nullptr;
-    GamemastersWidget *gamemastersWidget = nullptr;
+    GamemaStersWidget *gamemaStersWidget = nullptr;
     ColdStakingWidget *coldStakingWidget = nullptr;
     GovernanceWidget* governancewidget{nullptr};
     SettingsWidget* settingsWidget = nullptr;
