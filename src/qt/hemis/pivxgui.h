d27e9dae667bd2f14645753f97290ae703d95932
Alastair Clark <admin@alastair.guru>
2023-08-22 03:54:27 -0400

    Typo

--- src/qt/hemis/pivxgui.h
+++ src/qt/hemis/pivxgui.h
@@ -109,7 +109,7 @@ public Q_SLOTS:
     void goToSend();
     void goToReceive();
     void goToAddresses();
-    void goToGamemaSters();
+    void goToGamemasters();
     void goToGovernance();
     void goToColdStaking();
     void goToSettings();
@@ -180,7 +180,7 @@ private:
     SendWidget *sendWidget = nullptr;
     ReceiveWidget *receiveWidget = nullptr;
     AddressesWidget *addressesWidget = nullptr;
-    GamemaStersWidget *gamemaStersWidget = nullptr;
+    GamemastersWidget *gamemastersWidget = nullptr;
     ColdStakingWidget *coldStakingWidget = nullptr;
     GovernanceWidget* governancewidget{nullptr};
     SettingsWidget* settingsWidget = nullptr;
