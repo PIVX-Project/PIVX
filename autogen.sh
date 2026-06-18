fc594c625d668d3f7a563cb9ba5e37585707ddf2
Fuzzbawls <fuzzbawls@gmail.com>
2026-06-18 02:26:33 -0700

    Build: Patch for newer autoreconf
    Newer versions of autoreconf will deny manual changes to input files even if the `--force` option is given, instead favoring a new `--replace-handwritten` flag.
    
    This check is necessary as we use a sed patch for the chiabls build config file when using autotools.

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@
 
 export LC_ALL=C
 set -e
+
 srcdir="$(dirname $0)"
 cd "$srcdir"
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="$(command -v glibtoolize)"; then
@@ -13,4 +14,11 @@ if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="$(command -v glibtoolize)"; then
 fi
 command -v autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
-autoreconf --install --force --warnings=all
+
+if autoreconf --help | grep -q -- "--replace-handwritten"; then
+  echo "--replace-handwritten supported"
+  autoreconf --verbose --replace-handwritten --install --force --warnings=all
+else
+  echo "--replace-handwritten NOT supported"
+  autoreconf --verbose --install --force --warnings=all
+fi
