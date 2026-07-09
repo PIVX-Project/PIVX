ef3ff42a54450723bd4c43ad8b6fcfb780f1b6ba
Liquid369 <Liquid369@gmail.com>
2026-07-09 18:45:42 +0700

    Set testnet v6.0 activation height to block 5000

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -454,8 +454,7 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_V5_3].nActivationHeight          = 201;
         consensus.vUpgrades[Consensus::UPGRADE_V5_5].nActivationHeight          = 201;
         consensus.vUpgrades[Consensus::UPGRADE_V5_6].nActivationHeight          = 201;
-        consensus.vUpgrades[Consensus::UPGRADE_V6_0].nActivationHeight =
-                Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
+        consensus.vUpgrades[Consensus::UPGRADE_V6_0].nActivationHeight = 5000;
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
